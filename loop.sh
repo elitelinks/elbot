d4f5f6b7627ca3b445d4e983f8691bd1526f4fcf
plus <plus20charisma@gmail.com>
2016-05-06 07:34:36 -0700

    Fix files

--- loop.sh
+++ loop.sh
@@ -1,8 +1,8 @@
-#! /bin/bash
+#!/bin/bash
 while true
 do
-printf "\n+------\n------+\n+------\n+\n---------\n Starting red startup loop. ctrl+c twice quickly to stop. May also have to logout bot on discord.\n ---------\n+------\n------+\n+------\n+------\n"
-node bot\
-printf "\n+\n+\n----Bot stopped. If you haven't closed your terminal in a week, you should probably close it and reopen it----\n+\n+\n"
+printf "\n+------\n------+\n+------\n+\n---------\nStarting el startup loop. ctrl+c twice quickly to stop. May also have to logout bot on discord.\n ---------\n+------\n------+\n+------\n+------\n"
+node bot.js
+printf "\n+\n+\n----Bot stopped. ----\n+\n+\n"
 sleep 4
 done
